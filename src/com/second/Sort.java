dde069e2c762dd24a871a2fbea9477393c151ad9
jac <flybear0930@qq.com>
2019-11-06 16:17:43 +0800

    sort

--- src/com/second/Sort.java
+++ src/com/second/Sort.java
@@ -7,16 +7,20 @@ import edu.princeton.cs.algs4.Stopwatch;
 public class Sort {
     public static void main(String[] args) {
 
-        var array = new int[]{1,4,3,2,5,6};
-        //var randomInput = SortCompare.timeRandom-Input
-        new Sort().SortCompare(10,10,true);
-//        for (int i : array) {
-//           System.out.println(i);
-//        }
-        //array
+        var num = 100000;
+        var t = 10;
+        var isShow = false;
+        var sortWay = "Bubble,Insert,Select";
+        new Sort().SortCompare(num, t, isShow, sortWay);
     }
-    //选择排序:每个元素都进行比较
-    public void SelectSort(int[] array){
+
+    /**
+     * 选择排序
+     * 与每个元素进行比较，将需要的元素放到首位
+     *
+     * @param array int数组
+     */
+    private void SelectSort(int[] array) {
         for (int i = 0; i <array.length ; i++) {
             var max = i;
             for (int j = i + 1; j < array.length; j++) {
@@ -30,8 +34,38 @@ public class Sort {
             array[max]=temp;
         }
     }
-    //插入排序，每次循环都进行相邻比较
-    public void InsertSort(int[] array){
+
+    /**
+     * 冒泡排序
+     * 冒泡排序的基本思想是，对相邻的元素进行两两比较，顺序相反则进行交换，
+     * 这样，每一趟会将最小或最大的元素“浮”到顶端，最终达到完全有序
+     *
+     * @param array int 数组
+     */
+    private void BubbleSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            var isChange = false;
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] < array[j + 1]) {
+                    var temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                    isChange = true;
+                }
+            }
+            if (!isChange) {
+                break;
+            }
+        }
+    }
+
+    /**
+     * 插入排序
+     * 将新的元素插入已经排好序的数组之中
+     *
+     * @param array int数组
+     */
+    private void InsertSort(int[] array) {
         for (int i = 1; i <array.length; i++) {
             for (int j = i; j >0&&array[j]>array[j-1] ; j--) {
                 var temp = array[j];
@@ -45,9 +79,9 @@ public class Sort {
      * 希尔排序，基本插入排序的一种升级
      * 希尔排序为了加快速度简单地改进了插入排序， 交换不相邻的元素以对数组的局部
      * 进行排序， 并最终用插入排序将局部有序的数组排序
-     * @param array
+     * @param array int数组
      */
-    public void HIllSort(int[] array){
+    private void HIllSort(int[] array) {
         int n = array.length;
         int h = 1 ; //比较的跨度
         while (h<n/3) h=3*h+1;
@@ -70,38 +104,48 @@ public class Sort {
      * @param num 数组长度
      * @param t 比较次数
      * @param isShow 是否显示数组
+     * @param sortWay  排序方式
      */
-    public void SortCompare(int num,int t,boolean isShow){
+    private void SortCompare(int num, int t, boolean isShow, String sortWay) {
 
         for (int j = 0; j < t; j++) {
             int[] arr = new int[num];
             for (int i = 0; i < num; i++) {
                 arr[i] = (int) (Math.random() * num);
             }
+            if (isShow) print(arr);
+            if (sortWay.contains("Hill")) {
+                var array = arr.clone();
+                Stopwatch sw = new Stopwatch();
+                HIllSort(array);
+                if (isShow) print(array);
+                System.out.println(String.format("\n希尔排序的时间：%f", sw.elapsedTime()));
+            }
+            if (sortWay.contains("Insert")) {
+                var array = arr.clone();
+                Stopwatch sw = new Stopwatch();
+                InsertSort(array);
+                if (isShow) print(array);
+                System.out.println(String.format("\n插入排序的时间：%f", sw.elapsedTime()));
 
-            var arr2 = arr.clone();
-            if(isShow){
-                print(arr);
-            };
+            }
+            if (sortWay.contains("Bubble")) {
+                var array = arr.clone();
+                Stopwatch sw = new Stopwatch();
+                BubbleSort(array);
+                if (isShow) print(array);
+                System.out.println(String.format("\n冒泡排序的时间：%f", sw.elapsedTime()));
 
-            Stopwatch iw = new Stopwatch();
-            HIllSort(arr2);
-            System.out.println(String.format("\n希尔排序的时间：%f",iw.elapsedTime()));
-            if(isShow)print(arr2);
+            }
+            if (sortWay.contains("Select")) {
+                var array = arr.clone();
+                Stopwatch sw = new Stopwatch();
+                SelectSort(array);
+                if (isShow) print(array);
+                System.out.println(String.format("\n选择排序的时间：%f", sw.elapsedTime()));
+            }
 
-            continue;
-//
-////        Stopwatch iw = new Stopwatch();
-////            long start = System.currentTimeMillis();
-//            InsertSort(arr2);
-////        System.out.println(String.format("\n插入排序的时间：%f",iw.elapsedTime()));
-//            var now = System.currentTimeMillis();
-//            System.out.println(String.format("\n插入排序的时间：%s", (now - start) / 1000.0));
-//
-//            Stopwatch sw = new Stopwatch();
-//            SelectSort(arr);
-//            System.out.println(String.format("\n选择排序的时间：%s", sw.elapsedTime()));
-//            System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         }
 
 
@@ -109,12 +153,13 @@ public class Sort {
 
     /**
      * 打印数组
-     * @param array
+     * @param array int数组
      */
-    public void print(int[] array){
+    private void print(int[] array) {
         for (int i : array) {
            System.out.print(i+",");
         }
         System.out.print('\n');
     }
+
 }
